e0b416831f2354a9ff5da92d172e93ffdbe8ffdd
PolliHam <polina.shenets@gmail.com>
2018-11-21 02:40:00 +0300

    workers

---
+++ worker.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let lock = 15;
+
+function writeToFile(path, x){
+    setInterval(function() {
+        if(lock!==0){
+            fs.appendFile(path, Math.floor(Math.random()*15+12)+",  ", ()=>{});
+            lock--;
+        }
+    }, x);
+}
+
+writeToFile(process.argv[2], process.argv[3]);
